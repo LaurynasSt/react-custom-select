b8a5a37dfbb65054ab936af4ae6c008f46e2abf5
orezdrah <l.stackunas@gmail.com>
2021-03-29 16:52:54 +0300

    change from class component to hooks

--- src/app/index.js
+++ src/app/index.js
@@ -1,32 +1,54 @@
-import React from "react";
-import Select from "./components/Select";
-
+import { createContext, useContext, useEffect, useRef, useState } from "react";
 import "./index.css";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showList: false,
-      defaultSelectText: "Please select an option",
-      optionsList: [
-        { id: 1, name: "Option1" },
-        { id: 2, name: "Option2" },
-        { id: 3, name: "Option3" },
-      ]
-    };
-  }
-  render() {
-    return (
-      <div className="App">
-        <div className="test">
-          <Select
-            defaultText={this.state.defaultSelectText}
-            optionsList={this.state.optionsList}
-          />
-        </div>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Select onOptionChange={(value) => console.log(value)}>
+        <Option value={1}>Option 1 </Option>
+        <Option value={2}>Option 2 </Option>
+      </Select>
+    </div>
+  );
 }
- export default App;
+
+const SelectContext = createContext()
+
+const Select = ({ children, onOptionChange }) => {
+  //initializing state
+  const [selectedOption, setSelectedOption] = useState(0);
+  //memoizing callback inside ref object to avoid useEffect dependency
+  const cbRef = useRef(onOptionChange);
+  //calling outside callback to pass data about selected option
+  useEffect(() => {
+    if (selectedOption) {
+      cbRef.current(selectedOption);
+    }
+  }, [selectedOption])
+  //defining selector function
+  const selectOption = (option) => setSelectedOption(option)
+  return (
+    <SelectContext.Provider value={selectOption}>
+      {children}
+      <p>Selected option is : {selectedOption}</p>
+    </SelectContext.Provider>
+  )
+}
+
+const useSelectOption = () => {
+  //extracting useContext call into declarative custom hook
+  const ctx = useContext(SelectContext);
+  if (!ctx) {
+    throw new Error("useSelectOption may only be used within SelectContextProvider")
+  }
+  return ctx
+}
+
+const Option = ({ children, value }) => {
+  const selectOption = useSelectOption()
+  return (
+    <button onClick={() => selectOption(value)}>{children}</button>
+  )
+}
+
+export default App;
